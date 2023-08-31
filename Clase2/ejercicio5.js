0575c4b3a61193218f81f284cf0294d3cee53655
CeBarrera <barreracesar86@gmail.com>
2023-08-31 12:36:09 -0300

    Se realizó el Ejercicio 5 de la Clase 2.

---
+++ Clase2/ejercicio5.js
@@ -0,0 +1,40 @@
+// Red Social
+
+var perfil = {
+    nombre: 'Cesar',
+    edad: 36,
+    amigos: [
+        {
+            nombre: 'Laura',
+            edad: 35,
+        },
+        {
+            nombre: 'Myrian',
+            edad: 38,
+        },
+        {
+            nombre: 'Alexis',
+            edad: 26,
+        },
+        {
+            nombre: 'Claudio',
+            edad: 36,
+        },
+    ],
+};
+
+function masJoven(array) {
+    var menor = array[0];
+    for (let element of array) {
+        if(element.edad < menor.edad){
+            menor = element;
+        }
+    }
+    return menor.nombre;
+};
+
+// Muestra el nombre del amigo más joven
+const btn5 = document.querySelector('#btn5');
+btn5.addEventListener('click', function (event) {
+    console.log("El nombre es: " + masJoven(perfil.amigos));
+});
