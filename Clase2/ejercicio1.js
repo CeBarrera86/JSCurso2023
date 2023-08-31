9340e6f853fe61f0421d499e4a39144354a5c912
CeBarrera <barreracesar86@gmail.com>
2023-08-30 21:44:17 -0300

    Se crearon los ficheros para las distintas actividades.

---
+++ Clase2/ejercicio1.js
@@ -0,0 +1,25 @@
+var usuario = {
+    nombre: "Cesar",
+    peliculasFavoritas: [
+        {
+            titulo: 'Minions',
+            anio: 2015,
+        },
+        {
+            titulo: 'Titanic',
+            anio: 1997,
+        },
+        {
+            titulo: 'Avatar',
+            anio: 2009,
+        }
+    ],
+};
+
+const btn1 = document.querySelector('#btn1');
+btn1.addEventListener('click', function(event) {
+    console.log(usuario.peliculasFavoritas[1].titulo);
+});
+
+// console.log(usuario.peliculasFavoritas[1].titulo);
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # JSCurso2023
 Curso: "JavaScript Desarrollador Avanzado"
+
+
+En éste primer commit queda realizada la primera actividad del Curso, Clase 2.
