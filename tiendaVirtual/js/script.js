c8608546e5144d8eff835c9662cead6bd866cd29
CeBarrera <barreracesar86@gmail.com>
2023-09-23 11:20:02 -0300

    Tarea para sabado 23/09

--- tiendaVirtual/js/script.js
+++ tiendaVirtual/js/script.js
@@ -1,18 +1,19 @@
 class Producto {
-    constructor(nombre, precio, stock) {
+    constructor(nombre, precio, stock, tipo) {
         this.nombre = nombre;
         this.precio = precio;
         this.stock = stock;
+        this.tipo = tipo;
     }
 
     getInformacionProducto() {
-        return "Nombre: " + this.nombre + " | Precio: " + this.precio + " | Stock " + this.stock + " ";
+        return "Nombre: " + this.nombre + " | Precio: " + this.precio + " | Stock " + this.stock + " | Tipo " + this.tipo + " ";
     }
 }
 
 class ProductoElectronico extends Producto {
-    constructor(nombre, precio, stock, potencia) {
-        super(nombre, precio, stock);
+    constructor(nombre, precio, stock, tipo, potencia) {
+        super(nombre, precio, stock, tipo);
         this.potencia = potencia;
     }
 
@@ -24,10 +25,10 @@ class ProductoElectronico extends Producto {
 class ProductoAlimenticio extends Producto {
     fechaVencimiento;
     alimentoPerecedero;
-    constructor(nombre, precio, stock, fechaVencimiento, alimentoPerecedero) {
-        super(nombre, precio, stock)
-        this.fechaVencimiento = this.fechaVencimiento;
-        this.alimentoPerecedero = this.alimentoPerecedero;
+    constructor(nombre, precio, stock, tipo, fechaVencimiento, alimentoPerecedero) {
+        super(nombre, precio, stock, tipo);
+        this.fechaVencimiento = fechaVencimiento;
+        this.alimentoPerecedero = alimentoPerecedero;
     }
 
     tipoAlimento() {
@@ -60,28 +61,34 @@ class Carrito {
         return this.productos.reduce((a, b) => a + parseInt(b.precio), 0);
     }
 
+    vaciarCompleto() {
+        this.productos.splice(0, this.productos.length);
+    }
 }
 
 //inicio de variables
 const carrito = new Carrito();
 
-function agregarAlCarrito(index) {
+function agregarAlCarrito(elemento) {
+    let index = $(elemento).attr("data-id");
     let nombreProducto = $("#producto_" + index + " .nombre").text();
     let precio = $("#producto_" + index + " .precio").text();
 
     if ($("#producto_" + index).hasClass("electronico")) {
         let potencia = $("#producto_" + index + " .potencia").text();
-        let p1 = new ProductoElectronico(nombreProducto, precio, 1, potencia);
+        let tipoProducto = $(elemento).attr("data-tipo");
+        let p1 = new ProductoElectronico(nombreProducto, precio, 1, tipoProducto, potencia);
         carrito.agregarProducto(p1);
     } else {
         let fechaVencimiento = $("#producto_" + index + " .caducidad").text();
         let alimentoPerecedero = true;
-        let p2 = new ProductoAlimenticio(nombreProducto, precio, 1, fechaVencimiento, alimentoPerecedero);
+        let tipoProducto = $(elemento).attr("data-tipo");
+        let p2 = new ProductoAlimenticio(nombreProducto, precio, 1, tipoProducto, fechaVencimiento, alimentoPerecedero);
         carrito.agregarProducto(p2);
     }
+
     mostrarCarrito();
 }
-
 
 function mostrarCarrito() {
     const listaCarrito = $("#lista-carrito");
@@ -98,12 +105,23 @@ function mostrarCarrito() {
         listaCarrito.append(li);
     });
 
+    if (carrito.productos.length > 0) {
+        $("#vaciar").show();
+    } else {
+        $("#vaciar").hide();
+    }
+   
     $("#cantidad").html(carrito.productos.length);
     $("#total").html(carrito.calcular_total());
 }
 
 function eliminarDelCarrito(index) {
     carrito.eliminarProducto(index);
+    mostrarCarrito();
+}
+
+function vaciarCarrito() {
+    carrito.vaciarCompleto();
     mostrarCarrito();
 }
 
